2e58b6e5cfd233c6e1c04248fdb4509bb411c697
b00men <b00men@a947e8b4-fe6c-47dd-8ec2-89dcfceacaf9>
2012-11-09 16:36:57 +0000

    Some work with lesson5. TODO: Change type register by cpu time to real time or simple operation.

--- lessons/season_2/lesson5/programm.cpp
+++ lessons/season_2/lesson5/programm.cpp
@@ -9,27 +9,53 @@ void genvec_rand (int &mass_size, int *&mass);		// Generating rand vec.
 void genvec_anti (int &mass_size, int *&mass);		// Generating anti vec.
 void genvec_half (int &mass_size, int *&mass);		// Generating half rand vec.
 void swap (int &a, int &b);							// Swap two int el
-void sort (int &mass_size, int *&mass, int &no);	// Sort vecor. Use 'VSTAVKA' method. Depend on swap.
+double sort (int &mass_size, int *&mass);			// Sort vecor. Use 'VSTAVKA' method. Return time of work (NO!) in sec. Depend on swap.
+void math (int &MofMS_size, int *&MofMS, int *&mass, int &type, int &no);
+								// Count middle score of sort some(mass,type) vector some(MofMS) size in some(no) iterration in second.
+								// Type of genvec: 1 - rand, 2 - anti, 3 - halfrand.
 
 int main( )
 {
-	int mass_size, *mass, no=0; // 'no' for calculate system resource usage.
-	genvec_rand(mass_size,mass);
-	for (int i=0;i<mass_size;cout<<mass[i]<<' ',i++); cout<<'\n'; // while wthout out
-	genvec_anti(mass_size,mass);
-	for (int i=0;i<mass_size;cout<<mass[i]<<' ',i++); cout<<'\n'; // while wthout out
-	sort(mass_size,mass,no);
-	// for (int i=0;i<mass_size;cout<<mass[i]<<' ',i++); cout<<'\n'; // while wthout out
-	delete [] mass;
+	int MofMS_size, no=10, *mass, *MofMS; // MofMS - mass of mass_size, 'no' for numbers of iterations
+	srand(time(NULL));
+	readvec(MofMS_size,MofMS);
+	cout<<"Numbers of iterations: "<<no<<'\n';
+	for (int i=1; i<4; math (MofMS_size,MofMS,mass,i,no), i++);
 	return 0;
 }
 
-void sort (int &mass_size, int *&mass, int &no)
+void math (int &MofMS_size, int *&MofMS, int *&mass, int &type, int &no)
 {
+	double t=0;
+	switch(type)
+	{
+	case (1): cout<<"Rand vec:\n"; break;
+	case (2): cout<<"Anti vec:\n"; break;
+	case (3): cout<<"Half rand vec:\n"; break;
+	default: cout<<"Select type of gen vector!\n"; return;
+	}
+	for (int i=0; i<MofMS_size; i++)
+	{
+		cout<<"Mass_size: "<<MofMS[i]<<" Time in sec: ";
+		// little ПОНТЫ
+		for (int j=0; j<no; (type==1)?(genvec_rand(MofMS[i],mass)):((type==2)?(genvec_anti(MofMS[i],mass)):(genvec_half(MofMS[i],mass))), t=t+sort(MofMS[i],mass)/CLOCKS_PER_SEC, j++);
+		t=t/no;
+		cout<<t<<'\n';	
+	}
+	cout<<'\n';
+	return;
+}
+
+double sort (int &mass_size, int *&mass)
+{
+	clock_t start=clock();
 	for (int i=1; i<mass_size; i++)
 	{
 		for (int j=i-1; ((j>-1)&&(mass[j]>mass[j+1])); swap(mass[j],mass[j+1]), j--);
 	}
+	//for (int i=0;i<mass_size;cout<<mass[i]<<' ',i++); cout<<'\n'; // out to stdout
+	delete [] mass;
+	return (clock()-start);
 }
 
 void readvec (int &mass_size, int *&mass)
@@ -45,38 +71,27 @@ void readvec (int &mass_size, int *&mass)
 
 void genvec_rand (int &mass_size, int *&mass)
 {
-	ifstream in;
-	in.open( "massin" );
-	in>>mass_size;
-	in.close();
 	mass = new int [mass_size];
-	for (int i=0;i<mass_size;mass[i]=(rand() % (mass_size * 3) ), i++);
+	for (int i=0;i<mass_size;mass[i]=(rand() % (mass_size * 10) ), i++);
 	return;
 }
 
 
 void genvec_anti (int &mass_size, int *&mass)
 {
-	ifstream in;
-	in.open( "massin" );
-	in>>mass_size;
-	in.close();
 	mass = new int [mass_size];
-	for (int i=mass_size-1;i>-1;mass[i]=i, i++);
+	for (int i=0;i<mass_size;mass[i]=mass_size-i, i++);
 	return;
 }
-
 
 void genvec_half (int &mass_size, int *&mass)
 {
-	ifstream in;
-	in.open( "massin" );
-	in>>mass_size;
-	in.close();
 	mass = new int [mass_size];
-	for (int i=0;i<mass_size;mass[i]=(rand() % (mass_size * 3) ), i++);
+	for (int i=0;i<mass_size/2;mass[i]=i, i++);
+	for (int i=mass_size/2;i<mass_size;mass[i]=(rand() % (mass_size * 9) + mass_size/2), i++);
 	return;
 }
+
 void swap (int &a, int &b)
 {
 	int c;
